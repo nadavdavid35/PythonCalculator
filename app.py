6798f14bd142fc7bffa86fe11a4ef81f957eae46
nadavdavid35 <nadavd2005@gmail.com>
2025-04-11 14:52:57 +0300

    Adding division

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ n2 = int(input("Enter Second Number: "))
 
 sum = n1 + n2
 sub = n1 - n2
-multi = n1 * n2 
+mul = n1 * n2 
+div = n1 / n2
 
 print(f"{n1} + {n2} = {sum}")
 print(f"{n1} - {n2} = {sub}")
-print(f"{n1} * {n2} = {multi}")
+print(f"{n1} * {n2} = {mul}")
+print(f"{n1} / {n2} = {div}")
