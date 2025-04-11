c21a01df90c815a58e2cc47a552cd501d13f9486
nadavdavid35 <nadavd2005@gmail.com>
2025-04-11 18:46:56 +0300

    Adding sum and sub

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ print("--------------------------------")
 n1 = int(input("Enter First Number: "))
 n2 = int(input("Enter Second Number: "))
 
-
+sum = n1 + n2
+sub = n1 - n2
 mul = n1 * n2 
 div = n1 / n2
 
-
+print(f"{n1} + {n2} = {sum}")
+print(f"{n1} - {n2} = {sub}")
 print(f"{n1} * {n2} = {mul}")
 print(f"{n1} / {n2} = {div}")
+
+print("Hello World")
